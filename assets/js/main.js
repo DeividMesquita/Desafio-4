69f19db0cebcddfa364408734bad2a1220bfb2bd
DeividMesquita <deividsond08@gmail.com>
2024-11-27 13:57:51 -0300

    feat: galeria, modal personagens e ajustes na section dos personagens

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+//MODAL//
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
+
+
+
+
+
+
+//FANCYBOX//
+Fancybox.bind("[data-fancybox='gallery']", {
+    infinite: true,
+    Toolbar: {
+      display: [
+        { id: "back", label: "Voltar", position: "left" }, 
+        "close", 
+      ],
+    },
+    callbacks: {
+      on: {
+        init: (fancybox) => {
+    
+          fancybox.$container.querySelector("[data-fancybox-back]").addEventListener("click", () => {
+            window.history.back();
+          });
+        },
+      },
+    },
+  });
